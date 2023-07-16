4ffdddb2eb7e86c00f2b4f8bc2350b58cb4aad87
Zakariae007 <z.barakat@alumni.aui.ma>
2023-07-16 20:25:23 +0000

    add: add new student

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -16,5 +17,15 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) throws IllegalAccessException {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentByEmail.isPresent()){
+            throw new IllegalAccessException("Email Taken");
+        }
+
+        studentRepository.save(student);
     }
 }
